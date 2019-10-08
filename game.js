bab1f4476b3bcc618e3a97d7032ca6472326c05a
Bowen Boyd <bdb253@nau.edu>
2019-10-07 22:26:17 -0700

    Game loop and Menus
    I added a start Menu and implemented a game loop. Get to the ladder to go to the next level.

--- game.js
+++ game.js
@@ -20,15 +20,114 @@ var height = rows+1;
 
 var grid = [];
 var maze = [];
+var cells = [];
+var lives = 3;
 
 var start = new Point(8,8);
+var finish;
 var playerPos = start;
 
 
 function setup()
 {
-	let sheet = PIXI.Loader.shared.resources["spriteSheet.json"].spritesheet;
 	
+	let sheet = PIXI.Loader.shared.resources["spriteSheet.json"].spritesheet;
+
+	var Menu = new PIXI.Sprite(sheet.textures["wall1.png"]);
+	Menu.zIndex = 100;
+	Menu.scale.x = 11;
+	Menu.scale.y = 11;
+	Menu.sortableChildren = true;
+	stage.addChild(Menu);
+
+	//Title text
+	const titleStyle = new PIXI.TextStyle({
+		fontFamily: 'Arial',
+		fontSize: 65,
+		fontWeight: 'bold',
+		fill: '#FFC300',
+		strokeThickness: 5,
+	});
+
+	titleText = new PIXI.Text("Cave Dweller");
+	titleText.x = 15;
+	titleText.y = 50;
+	titleText.zIndex = 150
+	titleText.style = titleStyle;
+
+	//start text
+	const startStyle = new PIXI.TextStyle({
+		fontFamily: 'Arial',
+		fontSize: 45,
+		fontWeight: 'bold',
+		fill: '#FFC300',
+		strokeThickness: 5,
+	});
+
+	startText = new PIXI.Text("Start");
+	startText.x = 125;
+	startText.y = 170;
+	startText.zIndex = 150
+	startText.style = startStyle;
+	startText.interactive = true;
+	startText.click = function(e)
+	{
+		stage.removeChild(Menu);
+		stage.removeChild(startText);
+		stage.removeChild(titleText);
+		stage.removeChild(creditText);
+	};
+
+	creditText = new PIXI.Text("Credits");
+	creditText.x = 125;
+	creditText.y = 250;
+	creditText.zIndex = 150;
+	creditText.style = startStyle;
+	creditText.interactive = true;
+	creditText.click = function(e)
+	{
+		stage.removeChild(startText);
+		stage.removeChild(titleText);
+		stage.removeChild(creditText);
+		stage.addChild(backText);
+		stage.addChild(authorText);
+	}
+
+	backText = new PIXI.Text("Back");
+	backText.x = 10;
+	backText.y = 10;
+	backText.zIndex = 150;
+	backText.style = startStyle;
+	backText.interactive = true;
+	backText.click = function(e)
+	{
+		stage.removeChild(backText);
+		stage.removeChild(authorText);
+		stage.addChild(startText);
+		stage.addChild(titleText);
+		stage.addChild(creditText);
+	}
+
+	//author style
+	const authorStyle = new PIXI.TextStyle({
+		fontFamily: 'Arial',
+		fontSize: 35,
+		fontWeight: 'bold',
+		fill: '#FFC300',
+		strokeThickness: 5,
+	});
+
+	authorText = new PIXI.Text("Art, Sound and Design:\n Bowen Boyd");
+	authorText.x = 10;
+	authorText.y = 200;
+	authorText.zIndex = 150;
+	authorText.style =authorStyle;
+	
+	stage.addChild(startText);
+	stage.addChild(titleText);
+	stage.addChild(creditText);
+	
+
 	generate();
 	
 	//Cell constructor
@@ -52,6 +151,11 @@ function setup()
 		{
 			this.sprite = new PIXI.AnimatedSprite(sheet.animations["trap"]);
 			this.sprite.animationSpeed = 0.015;
+			this.walkable = true;
+		}
+		else if(type == "ladder")
+		{
+			this.sprite = new PIXI.Sprite(sheet.textures["ladder.png"]);
 			this.walkable = true;
 		}
 
@@ -89,6 +193,7 @@ function setup()
 		var move;
 		var possibleDir;
 		var pos = start;
+		var max = 0;
 
 		var moves = [];
 		moves.push(pos.y+(pos.x*width));
@@ -154,6 +259,11 @@ function setup()
 				pos.x = Math.floor(back / width);
 				pos.y = back % width;
 			}
+			if(moves.length > max)
+			{
+				finish = new Point(pos.x, pos.y);
+				max = moves.length;
+			}
 		}
 
 		for ( var x = 0; x < height; x++ )
@@ -169,18 +279,28 @@ function setup()
 						var cell = new Cell(x, y, "dirt");
 						cell.show()
 						grid[x].push(cell);
+						cells.push(cell);
+					}
+					else if(finish.x== x && finish.y == y)
+					{
+						var cell = new Cell(x, y, "ladder");
+						cell.show()
+						grid[x].push(cell);
+						cells.push(cell);
 					}
 					else if(chance == 0)
 					{
 						var cell = new Cell(x, y, "trap");
 						cell.show()
 						grid[x].push(cell);
+						cells.push(cell);
 					}
 					else
 					{
 						var cell = new Cell(x, y, "dirt");
 						cell.show()
 						grid[x].push(cell);
+						cells.push(cell);
 					}
 				}
 				else
@@ -188,18 +308,154 @@ function setup()
 					var cell= new Cell(x, y, "wall");
 					cell.show();
 					grid[x].push(cell);
+					cells.push(cell);
+				}
+			}
+		}		
+	}
+
+	var player = new PIXI.Sprite(sheet.textures["char.png"]);
+
+	player.position.x = start.x * cell_width+5;
+	player.position.y = start.y * cell_width+5;
+	player.zIndex = 15;
+	stage.addChild(player);
+
+	function keydownEventHandler(e)
+	{	
+		//w key
+		if(e.keyCode == 87)
+		{
+			if(grid[playerPos.x][playerPos.y-1].walkable)
+			{
+				var newy = player.position.y - 40;
+				createjs.Tween.get(player.position).to({x: player.position.x, y: newy}, 250);
+				playerPos.y-=1;
+			}
+			else
+			{
+				PIXI.sound.play('wrongWay');
+			}
+		}
+		//s key
+		if(e.keyCode == 83)
+		{
+			if(grid[playerPos.x][playerPos.y+1].walkable)
+			{
+				var newy = player.position.y + 40;
+				createjs.Tween.get(player.position).to({x: player.position.x, y: newy}, 250);
+				playerPos.y+=1;
+			}
+			else
+			{
+				PIXI.sound.play('wrongWay');
+			}
+		}
+		//a key
+		if(e.keyCode == 65)
+		{
+			if(grid[playerPos.x-1][playerPos.y].walkable)
+			{
+				var newX = player.position.x - 40;
+				createjs.Tween.get(player.position).to({x: newX, y: player.position.y}, 250);
+				playerPos.x -=1;
+			}
+			else
+			{
+				PIXI.sound.play('wrongWay');
+			}
+		}
+		//d key
+		if(e.keyCode == 68)
+		{
+			if(grid[playerPos.x+1][playerPos.y].walkable)
+			{
+				var newX = player.position.x + 40;
+				createjs.Tween.get(player.position).to({x: newX, y: player.position.y}, 250);
+				playerPos.x+=1;
+			}
+			else
+			{
+				PIXI.sound.play('wrongWay');
+			}
+		}
+
+
+	}
+
+	//gameOver text
+	const gameOverStyle = new PIXI.TextStyle({
+		fontFamily: 'Arial',
+		fontSize: 60,
+		fontWeight: 'bold',
+		fill: '#FF0000',
+		strokeThickness: 5,
+	});
+
+	gameOverText = new PIXI.Text("GAME OVER");
+	gameOverText.x = 20;
+	gameOverText.y = 175;
+	gameOverText.zIndex = 150;
+	gameOverText.style = gameOverStyle;
+
+	//restart text
+	const restartStyle = new PIXI.TextStyle({
+		fontFamily: 'Arial',
+		fontSize: 45,
+		fontWeight: 'bold',
+		fill: '#FF0000',
+		strokeThickness: 5,
+	});
+
+	restartText = new PIXI.Text("Restart?");
+	restartText.x = 125;
+	restartText.y = 250;
+	restartText.zIndex = 150;
+	restartText.style = restartStyle;
+	restartText.interactive = true;
+	restartText.click = function(e)
+	{
+		location.reload();
+	};
+
+	function animate()
+	{
+		requestAnimationFrame(animate);
+		renderer.render(stage);
+
+		for ( var x = 0; x < height; x++ )
+		{
+			for ( var y = 0; y < width; y++ )
+			{
+				//console.log("x: "+ x + ", y: " + y + "type: " + grid[x][y].type);
+				if(grid[x][y].type == "trap")
+				{
+					grid[x][y].sprite.play();
+					if(playerPos.x ==x && playerPos.y == y && grid[x][y].sprite.currentFrame != 0 && lives > 0 && grid[x][y].sprite.currentFrame !=4)
+					{
+						lives--;
+						PIXI.sound.play('wrongWay');
+						if(lives == 0)
+						{
+							stage.addChild(gameOverText);
+							stage.addChild(restartText);
+							stage.addChild(Menu);
+						}
+					}
+				}
+				else if(grid[x][y].type == "ladder" && playerPos.x ==x && playerPos.y == y)
+				{
+					generate();
+					player.position.x = start.x * cell_width;
+					player.position.y = start.y * cell_width;
+					playerPos = start;
 				}
 			}
 		}
 	}
 
-	var player = new PIXI.Sprite(sheet.textures["char.png"]);
-
-	player.position.x = start.x * cell_width;
-	player.position.y = start.y * cell_width;
-	player.zIndex = 15;
-	stage.addChild(player);
-
+	animate();
+	document.addEventListener('keydown', keydownEventHandler);
 }
 
 //Point data structure
@@ -215,93 +471,7 @@ function randInt(min, max)
 }
 
 
-function keydownEventHandler(e)
-{
-	//w key
-	if(e.keyCode == 87)
-	{
-		//console.log("x:" + playerPos.x);
-		//console.log('y:' + playerPos.y);
-		//console.log(grid[playerPos.x][playerPos.y-1]);
-		if(grid[playerPos.x][playerPos.y-1].walkable)
-		{
-			player.position.y -= 40;
-			//var newy = player.position.y - 40;
-			//createjs.Tween.get(player.position).to({x: player.position.x, y: newy}, 1000);
-			playerPos.y-=1;
-		}
-		else
-		{
-			PIXI.sound.play('wrongWay');
-		}
-	}
-	//s key
-	if(e.keyCode == 83)
-	{
-		//console.log("x:" + playerPos.x);
-		//console.log('y:' + playerPos.y);
-		//console.log(grid[playerPos.x][playerPos.y+1]);
-		if(grid[playerPos.x][playerPos.y+1].walkable)
-		{
-			player.position.y += 40;
-			playerPos.y+=1;
-		}
-		else
-		{
-			PIXI.sound.play('wrongWay');
-		}
-	}
-	//a key
-	if(e.keyCode == 65)
-	{
-		//console.log("x:" + playerPos.x);
-		//console.log('y:' + playerPos.y);
-		//console.log(grid[playerPos.x-1][playerPos.y]);
-		if(grid[playerPos.x-1][playerPos.y].walkable)
-		{
-			player.position.x -= 40;
-			playerPos.x -=1;
-		}
-		else
-		{
-			PIXI.sound.play('wrongWay');
-		}
-	}
-	//d key
-	if(e.keyCode == 68)
-	{
-		//console.log("x:" + playerPos.x);
-		//console.log('y:' + playerPos.y);
-		//console.log(grid[playerPos.x+1][playerPos.y]);
-		if(grid[playerPos.x+1][playerPos.y].walkable)
-		{
-			player.position.x += 40;
-			playerPos.x+=1;
-		}
-		else
-		{
-			PIXI.sound.play('wrongWay');
-		}
-	}
-}
 
-function animate()
-{
-	requestAnimationFrame(animate);
-	renderer.render(stage);
 
-	for ( var x = 0; x < height; x++ )
-	{
-		for ( var y = 0; y < width; y++ )
-		{
-			//console.log("x: "+ x + ", y: " + y + "type: " + grid[x][y].type);
-			if(grid[x][y].type == "trap")
-			{
-				grid[x][y].sprite.play();
-			}
-		}
-	}
-}
 
-animate();
-document.addEventListener('keydown', keydownEventHandler);
+
